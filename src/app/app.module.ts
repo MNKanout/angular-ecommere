a0b0d108e4eac83ad9027312a6a425e884baa792
MNKanout <34405548+MNKanout@users.noreply.github.com>
2023-08-30 16:44:23 +0200

    Added a shipping url to the shipping component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,10 +20,19 @@ import { ShippingComponent } from './shipping/shipping.component';
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
-      {path: 'cart', component: CartComponent},
+      { path: 'cart', component: CartComponent},
+      { path: 'shipping', component:ShippingComponent},
     ]),
   ],
-  declarations: [AppComponent, TopBarComponent, ProductListComponent, ProductAlertsComponent, ProductDetailsComponent, CartComponent, ShippingComponent],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent],
+    
   bootstrap: [AppComponent],
 })
 export class AppModule {}
