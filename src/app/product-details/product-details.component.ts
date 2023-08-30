15a3cba2f028666df17de6a47d2761e0b00da30f
MNKanout <34405548+MNKanout@users.noreply.github.com>
2023-08-30 11:19:28 +0200

    Created the method of getting the product id from the current link, and retrieving the the corresponding product.

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+import {Product, products} from '../products'
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) {}
+  
+  ngOnInit(){
+    // Get current route
+    const routeParam = this.route.snapshot.paramMap;
+    // Get the id of the product from the current route
+    const productIdFromRoute = Number(routeParam.get('productId'))
+
+    // Find the corresponding product to id
+    this.product = products.find(product => product.id === productIdFromRoute)
+
+
+  }
 
 }
