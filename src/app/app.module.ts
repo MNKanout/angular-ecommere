193150c2a6f20776a6304228aa9b553b80ca86a7
MNKanout <34405548+MNKanout@users.noreply.github.com>
2023-08-30 14:22:28 +0200

    Generated a cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   imports: [
@@ -18,7 +19,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
       { path: 'products/:productId', component: ProductDetailsComponent },
     ]),
   ],
-  declarations: [AppComponent, TopBarComponent, ProductListComponent, ProductAlertsComponent, ProductDetailsComponent],
+  declarations: [AppComponent, TopBarComponent, ProductListComponent, ProductAlertsComponent, ProductDetailsComponent, CartComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
