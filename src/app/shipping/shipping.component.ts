49962cde48ae95d11a0971a6f074adbb32094110
MNKanout <34405548+MNKanout@users.noreply.github.com>
2023-08-31 07:58:21 +0200

    Remove an unused dependency

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Observable, observable} from 'rxjs';
+import {Observable} from 'rxjs';
 import {CartService} from '../cart.service';
 
 
